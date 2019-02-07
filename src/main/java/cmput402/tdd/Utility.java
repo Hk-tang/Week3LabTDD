fff58211b623e25f83c283f438f81fa91aab0bcf
Hk-tang <hktang@ualberta.ca>
2019-02-06 18:08:41 -0700

    Added implementation for findTopScorer

--- src/main/java/cmput402/tdd/Utility.java
+++ src/main/java/cmput402/tdd/Utility.java
@@ -19,8 +19,20 @@ public class Utility {
 	}
 	
 	public HashMap<String, Integer>  findTopScorer(Search search){
+		HashMap<String, ArrayList<Integer>> results = search.returnAll();
+		HashMap<String, Integer> topScorer = new HashMap<String, Integer>();
 
-		return null;
+		for (String player : results.keySet()) {
+			Integer score = results.get(player).get(1);
+			if (topScorer.size() == 0) {
+				topScorer.put(player, score);
+			}
+			else if (score > topScorer.get(topScorer.keySet().iterator().next())) {
+				topScorer.clear();
+				topScorer.put(player, score);
+			}
+		}
+		return topScorer;
 	}
 	
 }
