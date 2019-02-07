bba21d3c67c0b5dc1d7da09550e3018e333e44f1
Hk-tang <hktang@ualberta.ca>
2019-02-06 18:00:38 -0700

    Completed countAverageScore test and implementation

--- src/main/java/cmput402/tdd/Utility.java
+++ src/main/java/cmput402/tdd/Utility.java
@@ -1,17 +1,24 @@
 package cmput402.tdd;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import cmput402.tdd.service.Search;
 
 public class Utility {
 
-	public HashMap<String, Integer>  countAverageScore(Search search){
-		
-		return null;
+	public HashMap<String, Float>  countAverageScore(Search search){
+		HashMap<String, ArrayList<Integer>> results = search.returnAll();
+		HashMap<String, Float> averages = new HashMap<String, Float>();
+
+		for (String player : results.keySet()) {
+			float avg = (float) results.get(player).get(1)/results.get(player).get(0);
+			averages.put(player, avg);
+		}
+		return averages;
 	}
 	
-public HashMap<String, Integer>  findTopScorer(Search search){
+	public HashMap<String, Integer>  findTopScorer(Search search){
 		
 		return null;
 	}
